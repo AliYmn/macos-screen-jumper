4d8d564214e031a7e3183f39fd45c8e388c902f0
Ali Yaman <aliymn.db@gmail.com>
2025-04-28 18:55:50 +0200

    Initial Commit

---
+++ jumper/jumperApp.swift
@@ -0,0 +1,17 @@
+//
+//  jumperApp.swift
+//  jumper
+//
+//  Created by aliymnx on 28.04.2025..
+//
+
+import SwiftUI
+
+@main
+struct jumperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
